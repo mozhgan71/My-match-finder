f6e648afe794fd2c7718e11f7576b50f994dcb22
Mozhgan71 <mozhganrajabali@gmail.com>
2024-05-22 21:55:11 +0330

    set app.component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit, PLATFORM_ID, inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./components/footer/footer.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { NgxSpinnerModule } from "ngx-spinner";
+import { AccountService } from './services/account.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, FooterComponent,
+    NavbarComponent, NgxSpinnerModule]
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  private accountService = inject(AccountService);
+  private platformId = inject(PLATFORM_ID); // used to test if we are in the client(browser) or server. We must be on the client to access localStorage!
+
+  ngOnInit(): void {
+    // console.log('PlatformId in OnInit:', this.platformId);
+    this.initUserOnPageRefresh();
+  }
+
+  initUserOnPageRefresh(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const loggedInUserStr = localStorage.getItem('loggedInUser');
+
+      if (loggedInUserStr) {
+        // First, check if user's token is not expired.
+        this.accountService.authorizeLoggedInUser();
+
+        // Then, set the authorized logged-in user
+        this.accountService.setCurrentUser(JSON.parse(loggedInUserStr))
+      }
+    }
+  }
 }
