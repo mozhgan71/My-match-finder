16319e84e0a9cb263d2a2d131ea661907f056b50
Mozhgan71 <mozhganrajabali@gmail.com>
2024-05-25 23:13:21 +0330

    Add auto-focus

---
+++ client/src/app/directives/auto-focus.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, ElementRef, inject } from '@angular/core';
+
+@Directive({
+  selector: '[autoFocusDir]',
+  standalone: true
+})
+export class AutoFocusDirective {
+  private elRef = inject(ElementRef);
+
+  name: string = '';
+
+  ngOnInit(): void {
+
+    this.name.toUpperCase();
+    setTimeout(() => {
+      this.elRef.nativeElement.focus();
+    }, 200);
+  }
+}
