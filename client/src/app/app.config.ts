2d2e033048fafae4c0a53a3aa8bc8eedf356f204
Mozhgan71 <mozhganrajabali@gmail.com>
2024-05-27 22:19:09 +0330

    Add Some Settings

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -3,8 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { jwtInterceptor } from './interseptors/jwt.interceptor';
+import { errorInterceptor } from './interseptors/error.interceptor';
+import { loadingInterceptor } from './interseptors/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimations(),
+    provideHttpClient(withInterceptors([jwtInterceptor, loadingInterceptor, errorInterceptor]))
+  ]
 };
